b4a1f81caf67d1581729be8414dc0ac4a8906bfa
albrdev <albrdev@gmail.com>
2019-05-28 18:52:51 +0200

    various tools

---
+++ Assets/Scripts/Tools/MathTools.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Xml.Schema;
+
+namespace Assets.Scripts.Tools
+{
+    public static class MathTools
+    {
+        public static float Neg(float value) { return value > 0f ? -value : value; }
+
+        public static sbyte Clamp(sbyte value, sbyte min, sbyte max) { return value < min ? min : (value > max ? max : value); }
+    }
+}
---
+++ Assets/Scripts/Tools/Unity/Vector2Tools.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Tools
+{
+    public static class Vector2Tools
+    {
+        public static float DotNormalized(Vector2 a, Vector2 b)
+        {
+            return (Mathf.Asin(Vector2.Dot(a.normalized, b.normalized)) / Mathf.PI) * 2;
+        }
+
+        public static Vector2 CrossNormalized(Vector2 a, Vector2 b)
+        {
+            return Vector2.Perpendicular(a.normalized - b.normalized).normalized;
+        }
+    }
+}
