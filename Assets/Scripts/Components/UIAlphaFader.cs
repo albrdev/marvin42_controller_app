8cf91c916c3be6650631426da50b10d504f8975c
albrdev <albrdev@gmail.com>
2019-05-30 01:03:27 +0200

    ui fader classed

---
+++ Assets/Scripts/Components/UIAlphaFader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Assets.Scripts.ExtensionClasses;
+
+namespace Assets.Scripts.Components
+{
+    [AddComponentMenu("GameObject/UI/UIAlphaFader")]
+    public class UIAlphaFader : UIFader
+    {
+        public void Fade(float value, float time, float delay = 0f, bool recursive = false, bool realtime = true)
+        {
+            foreach(var graphic in Graphics(recursive))
+            {
+                graphic.FadeAlpha(value, time, delay, realtime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Components/UIFader.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts.ExtensionClasses;
+
+namespace Assets.Scripts.Components
+{
+    public abstract class UIFader : MonoBehaviour
+    {
+        protected IEnumerable<Graphic> Graphics(bool recursive)
+        {
+            if(gameObject.activeSelf)
+            {
+                Graphic graphic = GetComponent<Graphic>();
+                if(graphic != null)
+                {
+                    yield return graphic;
+                }
+            }
+
+            if(recursive)
+            {
+                var graphics = GetComponentsInChildren<Graphic>();
+                foreach(Graphic graphic in graphics)
+                {
+                    yield return graphic;
+                }
+            }
+        }
+
+        public void SetAlpha(float value, bool recursive = false)
+        {
+            foreach(var graphic in Graphics(recursive))
+            {
+                graphic.canvasRenderer.SetAlpha(value);
+            }
+        }
+
+        public void StopFade(float value, bool recursive = false)
+        {
+            foreach(var graphic in Graphics(recursive))
+            {
+                graphic.StopFade();
+                graphic.canvasRenderer.SetAlpha(value);
+            }
+        }
+
+        public void StopFade(bool recursive = false)
+        {
+            foreach(var graphic in Graphics(recursive))
+            {
+                graphic.StopFade();
+            }
+        }
+    }
+}
